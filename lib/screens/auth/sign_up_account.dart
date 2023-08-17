7255ede51a211dd03134a32a99964982fda6fbe8
fawad kaleem <fwdkaleem@gmail.com>
2023-08-17 12:21:30 +0500

    signUp

--- lib/screens/auth/sign_up_account.dart
+++ lib/screens/auth/sign_up_account.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:partymania_users/screens/auth/login_screen.dart';
+import 'package:partymania_users/utils/button.dart';
+import 'package:partymania_users/utils/colors.dart';
+import 'package:partymania_users/utils/controllers.dart';
+import 'package:partymania_users/utils/textformfield.dart';
 
 class SignUpAccount extends StatefulWidget {
   const SignUpAccount({super.key});
@@ -10,6 +15,333 @@ class SignUpAccount extends StatefulWidget {
 class _SignUpAccountState extends State<SignUpAccount> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(
+                height: 60,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "Sign Up",
+                  style: TextStyle(
+                      fontSize: 24,
+                      color: textColor,
+                      fontWeight: FontWeight.w600),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  "SignUp into your account",
+                  style: TextStyle(
+                      fontSize: 14,
+                      color: textColor.withOpacity(.7),
+                      fontWeight: FontWeight.w400),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Full Name",
+                      style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormInputField(
+                      preIcon: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Image.asset(
+                          "assets/frame.png",
+                          width: 10,
+                          height: 10,
+                        ),
+                      ),
+                      textInputType: TextInputType.text,
+                      hintText: "Enter Full Name",
+                      controller: signUpFullNameController,
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Email",
+                      style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormInputField(
+                      preIcon: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Image.asset(
+                          "assets/email.png",
+                          width: 10,
+                          height: 10,
+                        ),
+                      ),
+                      textInputType: TextInputType.text,
+                      hintText: "Email Address",
+                      controller: signUpEmailController,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Phone Number",
+                      style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormInputField(
+                      preIcon: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Image.asset(
+                          "assets/call.png",
+                          width: 10,
+                          height: 10,
+                        ),
+                      ),
+                      textInputType: TextInputType.number,
+                      hintText: "424-232-000",
+                      controller: phoneNumberController,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Date of Birth",
+                            style: TextStyle(
+                                color: textColor,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 12),
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          TextFormInputField(
+                            suIcon: Padding(
+                              padding: const EdgeInsets.all(13.0),
+                              child: Image.asset(
+                                "assets/calendar.png",
+                                width: 10,
+                                height: 10,
+                              ),
+                            ),
+                            textInputType: TextInputType.text,
+                            hintText: "Select DOB",
+                            controller: clubStateController,
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 7,
+                    ),
+                    Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Gender",
+                            style: TextStyle(
+                                color: textColor,
+                                fontWeight: FontWeight.w400,
+                                fontSize: 12),
+                          ),
+                          const SizedBox(
+                            height: 7,
+                          ),
+                          TextFormInputField(
+                            suIcon: Padding(
+                              padding: const EdgeInsets.all(13.0),
+                              child: Image.asset(
+                                "assets/Vector.png",
+                                width: 10,
+                                height: 10,
+                              ),
+                            ),
+                            textInputType: TextInputType.text,
+                            hintText: "Select Gender",
+                            controller: clubCityController,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Create Password",
+                      style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormInputField(
+                      preIcon: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Image.asset(
+                          "assets/lock.png",
+                          width: 10,
+                          height: 10,
+                        ),
+                      ),
+                      textInputType: TextInputType.text,
+                      hintText: "Enter Password",
+                      controller: createPassword,
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Confrim Password",
+                      style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextFormInputField(
+                      preIcon: Padding(
+                        padding: const EdgeInsets.all(13.0),
+                        child: Image.asset(
+                          "assets/lock.png",
+                          width: 10,
+                          height: 10,
+                        ),
+                      ),
+                      textInputType: TextInputType.text,
+                      hintText: "Enter Password",
+                      controller: confrimPassword,
+                    )
+                  ],
+                ),
+              ),
+              Center(
+                child: Container(
+                  margin: EdgeInsets.only(bottom: 5, right: 12, left: 12),
+                  child: Text(
+                    "By tapping Sign up, you agree to our Terms and Policy",
+                    style: TextStyle(
+                        color: textColor.withOpacity(.7),
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 15,
+              ),
+              Center(
+                  child: SaveButton(
+                      title: 'Signup',
+                      onTap: () {
+                        // Navigator.push(
+                        //     context,
+                        //     MaterialPageRoute(
+                        //         builder: (builder) => CreateProfile()));
+                      })),
+              const SizedBox(
+                height: 15,
+              ),
+              Container(
+                margin: EdgeInsets.only(bottom: 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Already have an account?",
+                      style: TextStyle(
+                          color: textColor.withOpacity(.7),
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(
+                      width: 5,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (builder) => LoginScreen()));
+                      },
+                      child: Text(
+                        " Log In",
+                        style: TextStyle(
+                            color: textColor,
+                            fontSize: 14,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
