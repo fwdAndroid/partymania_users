415aab9787071bf7a4461b9a1784d3dbda971bc6
fawad kaleem <fwdkaleem@gmail.com>
2023-08-17 19:36:42 +0500

    Clubs Aassd

--- lib/screens/dashboardpages/club_page.dart
+++ lib/screens/dashboardpages/club_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:partymania_users/screens/clubtab/clubs.dart';
+import 'package:partymania_users/screens/clubtab/following.dart';
+import 'package:partymania_users/utils/colors.dart';
 
 class ClubPage extends StatefulWidget {
   const ClubPage({super.key});
@@ -10,6 +13,41 @@ class ClubPage extends StatefulWidget {
 class _ClubPageState extends State<ClubPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        backgroundColor: backgroundColor,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: backgroundColor,
+          bottom: TabBar(
+            labelColor: textColor,
+            unselectedLabelColor: textColor.withOpacity(.7),
+            indicatorSize: TabBarIndicatorSize.tab,
+            indicator: BoxDecoration(
+                borderRadius: BorderRadius.circular(9), color: otpColor),
+            tabs: [
+              Tab(
+                text: "Clubs",
+              ),
+              Tab(
+                text: "Following",
+              ),
+            ],
+          ),
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Clubs',
+            style: TextStyle(color: textColor),
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Clubs(),
+            Following(),
+          ],
+        ),
+      ),
+    );
   }
 }
