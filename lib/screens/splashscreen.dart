50ee0c5ab68b466b7d70e5e159d4808e8fa36184
fawad kaleem <fwdkaleem@gmail.com>
2023-08-17 10:35:25 +0500

    Splash Screen

---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,50 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:partymania_users/screens/auth/login_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage(
+            "assets/background.png",
+          ),
+          fit: BoxFit.cover,
+        )),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            margin: const EdgeInsets.only(left: 40, right: 40),
+            child: Image.asset(
+              "assets/logo.png",
+              width: 160,
+              height: 80,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
