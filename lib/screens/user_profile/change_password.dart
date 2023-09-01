d79e4389e6d9ebaea49cf7b450612cb8708b2077
fawad kaleem <fwdkaleem@gmail.com>
2023-09-01 05:52:36 +0500

    Change Pass

--- lib/screens/user_profile/change_password.dart
+++ lib/screens/user_profile/change_password.dart
@@ -1,8 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:partymania_users/screens/auth/login_screen.dart';
 import 'package:partymania_users/utils/button.dart';
 import 'package:partymania_users/utils/colors.dart';
-import 'package:partymania_users/utils/controllers.dart';
 import 'package:partymania_users/utils/textformfield.dart';
 
 class ChangePassword extends StatefulWidget {
@@ -13,108 +13,83 @@ class ChangePassword extends StatefulWidget {
 }
 
 class _ChangePasswordState extends State<ChangePassword> {
+  final _formKey = GlobalKey<FormState>();
+  final _auth = FirebaseAuth.instance;
+  bool _isLoading = false;
+  String _newPassword = '';
+  TextEditingController _changePass = TextEditingController();
+  TextEditingController _currentPassword = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(
         iconTheme: IconThemeData(color: textColor),
-        centerTitle: true,
-        title: Text(
-          "Change Password",
-          style: TextStyle(
-              color: textColor, fontWeight: FontWeight.w500, fontSize: 16),
-        ),
         backgroundColor: backgroundColor,
+        title: Text(
+          'Change Password',
+          style: TextStyle(color: textColor),
+        ),
       ),
-      backgroundColor: backgroundColor,
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Current Password",
-                  style: TextStyle(
-                      color: Color(0xffF9FAFB),
-                      fontWeight: FontWeight.w300,
-                      fontSize: 15),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormInputField(
-                  textInputType: TextInputType.text,
-                  hintText: "Current Password",
-                  controller: currentPass,
-                )
-              ],
-            ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              TextFormInputField(
+                textInputType: TextInputType.visiblePassword,
+                hintText: "Current Password",
+                controller: _currentPassword,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormInputField(
+                textInputType: TextInputType.visiblePassword,
+                hintText: "New Password",
+                controller: _changePass,
+                onChanged: (value) {
+                  _newPassword = value;
+                },
+                validat: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter a password';
+                  }
+                  return null;
+                },
+              ),
+              SizedBox(height: 16),
+              _isLoading
+                  ? Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : Center(
+                      child: SaveButton(
+                        onTap: _changePassword,
+                        title: 'Change Password',
+                      ),
+                    ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "New Passowrd",
-                  style: TextStyle(
-                      color: Color(0xffF9FAFB),
-                      fontWeight: FontWeight.w300,
-                      fontSize: 15),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormInputField(
-                  textInputType: TextInputType.text,
-                  hintText: "New Password",
-                  controller: newPassword,
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Confirm New Password",
-                  style: TextStyle(
-                      color: Color(0xffF9FAFB),
-                      fontWeight: FontWeight.w300,
-                      fontSize: 15),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextFormInputField(
-                  textInputType: TextInputType.text,
-                  hintText: "Confirm New Password",
-                  controller: newConfrimPassword,
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Center(
-              child: Container(
-                  margin: EdgeInsets.only(left: 15, right: 15, top: 10),
-                  child: SaveButton(
-                      title: "Change Password",
-                      onTap: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (builder) => LoginScreen()));
-                      })),
-            ),
-          ),
-        ],
+        ),
       ),
     );
+  }
+
+  void _changePassword() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await _auth.currentUser!.updatePassword(_newPassword);
+        // Password updated successfully
+        Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (builder) => LoginScreen()));
+        // You can show a success message or navigate to a different page
+      } catch (e) {
+        // Handle error
+        print('Error changing password: $e');
+      }
+    }
   }
 }
