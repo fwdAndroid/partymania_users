6e8e6e007dc36be29d9795b4c044dffbad911c7e
fawad kaleem <fwdkaleem@gmail.com>
2023-09-09 18:29:57 +0500

    Counter Created

--- lib/screens/tickettab/tickets.dart
+++ lib/screens/tickettab/tickets.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:partymania_users/screens/tickettab/ticket_payment.dart';
 import 'package:partymania_users/utils/button.dart';
 import 'package:partymania_users/utils/colors.dart';
+import 'package:partymania_users/utils/controllers.dart';
 
 class Tickets extends StatefulWidget {
   final uuid;
@@ -44,136 +45,167 @@ class Tickets extends StatefulWidget {
 }
 
 class _TicketsState extends State<Tickets> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
+  multivalue(int a, int b) {
+    int a = _counter;
+    int b = int.parse(widget.eventPrice);
+    int c = a * b;
+    return c;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
       body: Column(
         children: [
-          SizedBox(
-            height: 450,
-            child: ListView.builder(
-                itemCount: 3,
-                itemBuilder: (BuildContext context, int index) {
-                  return Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Column(
+          Padding(
+              padding: const EdgeInsets.all(4.0),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Row(
-                                      children: [
-                                        Text(
-                                          "Couple",
-                                          style: TextStyle(
-                                              color: textColor,
-                                              fontWeight: FontWeight.w400,
-                                              fontSize: 16),
-                                        ),
-                                        SizedBox(
-                                          width: 6,
-                                        ),
-                                        Text(
-                                          "Guestlist",
-                                          style: TextStyle(
-                                              color: textColor.withOpacity(.7),
-                                              fontWeight: FontWeight.w400,
-                                              fontSize: 12),
-                                        ),
-                                      ],
-                                    ),
-                                    Text(
-                                      "Before 9:30PM",
-                                      style: TextStyle(color: grey),
-                                    ),
-                                    Text(
-                                      "Free",
-                                      style: TextStyle(
-                                          color: textColor,
-                                          fontWeight: FontWeight.w400,
-                                          fontSize: 12),
-                                    ),
-                                    const SizedBox(
-                                      height: 6,
-                                    ),
-                                  ],
+                                Text(
+                                  widget.coupleValue
+                                      .toString()
+                                      .substring(6)
+                                      .toUpperCase(),
+                                  style: TextStyle(
+                                      color: textColor,
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 16),
                                 ),
-                                Row(
-                                  children: [
-                                    Container(
-                                      width: 45,
-                                      height: 45,
-                                      decoration: BoxDecoration(
-                                          color: otpColor,
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10))),
-                                      child: Center(
-                                        child: Text(
-                                          "-",
-                                          style: TextStyle(
-                                              color: textColor, fontSize: 30),
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 12,
-                                    ),
-                                    Container(
-                                      width: 45,
-                                      height: 45,
-                                      decoration: BoxDecoration(
-                                          color: backgroundColor,
-                                          border: Border.all(
-                                              color: textColor, width: .5),
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10))),
-                                      child: Center(
-                                        child: Padding(
-                                          padding: const EdgeInsets.all(8.0),
-                                          child: Text(
-                                            "2",
-                                            style: TextStyle(
-                                                color: textColor, fontSize: 20),
-                                          ),
-                                        ),
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      width: 12,
-                                    ),
-                                    Container(
-                                      width: 45,
-                                      height: 45,
-                                      decoration: BoxDecoration(
-                                          color: otpColor,
-                                          borderRadius: BorderRadius.all(
-                                              Radius.circular(10))),
-                                      child: Center(
-                                        child: Text(
-                                          "+",
-                                          style: TextStyle(
-                                              color: textColor, fontSize: 30),
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                )
+                                SizedBox(
+                                  width: 6,
+                                ),
+                                Text(
+                                  "Guestlist",
+                                  style: TextStyle(
+                                      color: textColor.withOpacity(.7),
+                                      fontWeight: FontWeight.w400,
+                                      fontSize: 12),
+                                ),
                               ],
                             ),
-                            Divider(
-                              color: textColor,
-                            )
+                            Row(
+                              children: [
+                                Text(
+                                  "Before ",
+                                  style: TextStyle(color: grey),
+                                ),
+                                Text(
+                                  widget.fromTime,
+                                  style: TextStyle(color: grey),
+                                ),
+                              ],
+                            ),
+                            Text(
+                              "Free",
+                              style: TextStyle(
+                                  color: textColor,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 12),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
                           ],
                         ),
-                      ));
-                }),
-          ),
+                        Row(
+                          children: [
+                            InkWell(
+                              onTap: _decrementCounter,
+                              child: Container(
+                                width: 45,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                    color: otpColor,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                child: Center(
+                                  child: Text(
+                                    "-",
+                                    style: TextStyle(
+                                        color: textColor, fontSize: 30),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 12,
+                            ),
+                            Container(
+                              width: 45,
+                              height: 45,
+                              decoration: BoxDecoration(
+                                  color: backgroundColor,
+                                  border:
+                                      Border.all(color: textColor, width: .5),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(10))),
+                              child: Center(
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    '$_counter',
+                                    style: TextStyle(
+                                        color: textColor, fontSize: 20),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 12,
+                            ),
+                            InkWell(
+                              onTap: _incrementCounter,
+                              child: Container(
+                                width: 45,
+                                height: 45,
+                                decoration: BoxDecoration(
+                                    color: otpColor,
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(10))),
+                                child: Center(
+                                  child: Text(
+                                    "+",
+                                    style: TextStyle(
+                                        color: textColor, fontSize: 30),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Divider(
+                      color: textColor,
+                    )
+                  ],
+                ),
+              )),
           Flexible(child: Container()),
           Divider(
             color: textColor,
@@ -184,18 +216,25 @@ class _TicketsState extends State<Tickets> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text(
-                  "Total Amount:",
+                  "Total Amount",
                   style: TextStyle(
                       color: textColor,
                       fontSize: 18,
                       fontWeight: FontWeight.w500),
                 ),
-                Text(
-                  "Rs. 7,000.00:",
-                  style: TextStyle(
-                      color: textColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w500),
+                InkWell(
+                  onTap: () {
+                    print(multivalue(_counter, int.parse(widget.eventPrice)));
+                  },
+                  child: Text(
+                    "Rs " +
+                        multivalue(_counter, int.parse(widget.eventPrice))
+                            .toString(),
+                    style: TextStyle(
+                        color: textColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500),
+                  ),
                 ),
               ],
             ),
