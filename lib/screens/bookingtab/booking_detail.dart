ccf1d12f68e44fe0226153ae9ddc7e6bc4539f57
fawad kaleem <fwdkaleem@gmail.com>
2023-09-25 11:57:26 +0500

    Issues Resolved

--- lib/screens/bookingtab/booking_detail.dart
+++ lib/screens/bookingtab/booking_detail.dart
@@ -1,8 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:partymania_users/utils/button.dart';
 import 'package:partymania_users/utils/colors.dart';
-import 'package:partymania_users/utils/longtext.dart';
 
 class BookingDetails extends StatefulWidget {
   final uuid;
