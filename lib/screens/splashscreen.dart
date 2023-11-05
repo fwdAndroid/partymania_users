25b39010a06e7bbf2f5d8e93f087e55af7e9eca3
fawad kaleem <fwdkaleem@gmail.com>
2023-11-05 06:27:12 +0500

    Gif Splash

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,50 +1,62 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:partymania_users/responsive/responsive_layout.dart';
 import 'package:partymania_users/screens/auth/login_screen.dart';
+import 'package:partymania_users/utils/colors.dart';
+import 'package:partymania_users/web_module/web_auth/web_login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+  SplashScreen({Key? key}) : super(key: key);
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  State<StatefulWidget> createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
   void initState() {
     super.initState();
-    Timer(
-        Duration(seconds: 3),
-        () => Navigator.pushReplacement(
-            context, MaterialPageRoute(builder: (context) => LoginScreen())));
+
+    check(context);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-          image: AssetImage(
-            "assets/background.png",
-          ),
-          fit: BoxFit.cover,
-        )),
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Container(
-            margin: const EdgeInsets.only(left: 40, right: 40),
-            child: Image.asset(
-              "assets/logo.png",
-              width: 160,
-              height: 80,
-            ),
-          ),
-        ),
+      backgroundColor: backgroundColor,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [Image.asset("assets/splash.gif")],
       ),
     );
+  }
+}
+
+check(BuildContext context) {
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  if (user != null) {
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ResponsiveLayout(
+                      mobileScreenLayout: LoginScreen(),
+                      webScreenLayout: WebLoginScreen(),
+                    ))));
+  } else {
+    Timer(
+        Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(
+                builder: (context) => ResponsiveLayout(
+                      mobileScreenLayout: LoginScreen(),
+                      webScreenLayout: WebLoginScreen(),
+                    ))));
   }
 }
---
+++ lib/web_module/web_auth/web_login_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WebLoginScreen extends StatefulWidget {
+  const WebLoginScreen({super.key});
+
+  @override
+  State<WebLoginScreen> createState() => _WebLoginScreenState();
+}
+
+class _WebLoginScreenState extends State<WebLoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
